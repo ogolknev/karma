d5911569d78810a07428ec9e4077d7dce651e983
ogolknev <ogolknevnv@ya.ru>
2025-07-30 23:23:33 +0300

    refactor(app): split app setup and server start into separate files

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,8 @@ import Elysia from "elysia";
 import { userRouter } from "./modules/user";
 import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia()
+export const app = new Elysia()
   .use(swagger({ provider: "swagger-ui" }))
   .use(userRouter)
-  .listen(process.env.PORT!);
+  
 
-console.log(`\x1b[32m✔\x1b[0m Server listening: \x1b[32m${app.server?.url}\x1b[0m`);
---
+++ src/server.ts
@@ -0,0 +1,5 @@
+import { app } from "./app";
+
+app.listen(process.env.PORT!);
+
+console.log(`\x1b[32m✔\x1b[0m Server listening: \x1b[32m${app.server?.url}\x1b[0m`);
