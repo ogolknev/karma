b826dda37dc761c6477f1c73cec99eb27e96b06f
ogolknev <ogolknevnv@ya.ru>
2025-07-30 21:54:28 +0300

    feat(user): add user router

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import Elysia from "elysia";
+import { userRouter } from "./modules/user";
 
-const app = new Elysia().listen(process.env.PORT!)
+const app = new Elysia()
+.use(userRouter)
+.listen(process.env.PORT!)
 
 console.log(`\x1b[32m✔\x1b[0m Server listening: \x1b[32m${app.server?.url}\x1b[0m`)
---
+++ src/modules/user/router.ts
@@ -0,0 +1,94 @@
+import Elysia, { t } from "elysia";
+import { DrizzleUserService, UserService } from "./service";
+import {
+  userCreateParamsSchema,
+  userCreateResponseSchema,
+  userGetOneParamsSchema,
+  userGetOneResponseSchema,
+  userGetManyParamsSchema,
+  userGetManyResponseSchema,
+  userUpdateParamsSchema,
+  userUpdateResponseSchema,
+  userDeleteParamsSchema,
+  userDeleteResponseSchema,
+} from "./model";
+
+const userService: UserService = new DrizzleUserService();
+
+export const userRouter = new Elysia({ prefix: "/users" })
+  .post(
+    "/register",
+    async ({ body }) => {
+      const response = await userService.create(body);
+
+      return response;
+    },
+    {
+      body: userCreateParamsSchema,
+      response: {
+        200: userCreateResponseSchema,
+      },
+    }
+  )
+  .get(
+    "/",
+    async ({ body }) => {
+      const response = await userService.get(body);
+
+      return response;
+    },
+    {
+      body: userGetManyParamsSchema,
+      response: {
+        200: userGetManyResponseSchema,
+      },
+    }
+  )
+  .guard({
+    response: {
+      404: t.String(),
+    },
+  })
+  .get(
+    "/:id",
+    async ({ params, status }) => {
+      const response = await userService.get(params);
+
+      return response ?? status(404, "User not found");
+    },
+    {
+      params: userGetOneParamsSchema,
+      response: {
+        200: userGetOneResponseSchema,
+      },
+    }
+  )
+  .patch(
+    "/:id",
+    async ({ params, body, status }) => {
+      const response = userService.update({ ...params, ...body });
+
+      return response ?? status(404, "User not found");
+    },
+    {
+      body: t.Omit(userUpdateParamsSchema, ["id"]),
+      params: t.Pick(userUpdateParamsSchema, ["id"]),
+      response: {
+        200: userUpdateResponseSchema,
+      },
+    }
+  )
+  .delete(
+    "/:id",
+    async ({ params, status }) => {
+      const response = await userService.delete(params);
+
+      return response ?? status(404, "User not found");
+    },
+    {
+      params: userDeleteParamsSchema,
+      response: {
+        200: userDeleteResponseSchema,
+      },
+    }
+  );
