a94962f7ed22d3aa19bb114458694c2ae298795b
ogolknev <ogolknevnv@ya.ru>
2025-07-30 22:15:39 +0300

    feat(app): add swagger plugin

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import Elysia from "elysia";
 import { userRouter } from "./modules/user";
+import { swagger } from "@elysiajs/swagger";
 
 const app = new Elysia()
-.use(userRouter)
-.listen(process.env.PORT!)
+  .use(swagger({ provider: "swagger-ui" }))
+  .use(userRouter)
+  .listen(process.env.PORT!);
 
-console.log(`\x1b[32m✔\x1b[0m Server listening: \x1b[32m${app.server?.url}\x1b[0m`)
+console.log(`\x1b[32m✔\x1b[0m Server listening: \x1b[32m${app.server?.url}\x1b[0m`);
