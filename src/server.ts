42e6ee18cd77a3bb63a6da482da911797883e930
ogolknev <ogolknevnv@ya.ru>
2025-07-31 08:50:41 +0300

    chore(server): update log message prefix to "Server listening on:"

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,4 @@ import { app } from "./app";
 
 app.listen(process.env.PORT!);
 
-console.log(`\x1b[32m✔\x1b[0m Server listening: \x1b[32m${app.server?.url}\x1b[0m`);
+console.log(`\x1b[32m✔\x1b[0m Server listening on: \x1b[32m${app.server?.url}\x1b[0m`);
