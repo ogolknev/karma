4ad1b32c5cae5b300de1fed7fe4d936b666c195f
ogolknev <ogolknevnv@ya.ru>
2025-08-23 23:48:07 +0300

    feat(core, user): switch User entity from interface to class

--- src/core/modules/user/user.entity.ts
+++ src/core/modules/user/user.entity.ts
@@ -1,5 +1,21 @@
-export interface User {
-  id: string
-  username: string
-  name: string
+import { generateId, hashPassword } from "@/shared/utils/crypto";
+
+export class User {
+  constructor(
+    public id: string,
+    public name: string,
+    public username: string,
+    private passwordHash: string
+  ) {}
+
+  static async create(name: string, username: string, password: string) {
+    const id = generateId();
+    const passwordHash = await hashPassword(password);
+
+    return new User(id, name, username, passwordHash);
+  }
+
+  getPasswordHash() {
+    return this.passwordHash;
+  }
 }
