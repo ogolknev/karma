862f4cbbd32a752fc77a68394c78da9786e854f3
ogolknev <ogolknevnv@ya.ru>
2025-08-26 14:48:43 +0300

    refactor(core, user): simplify UserUpdate to call repo.update directly

--- src/core/modules/user/uc/user.update.ts
+++ src/core/modules/user/uc/user.update.ts
@@ -1,21 +1,10 @@
 import { UserUpdateDTO } from "../dto";
-import { UserNotFoundError } from "../errors";
 import { UserRepo } from "../user.repo";
 
 export class UserUpdate {
   constructor(private userRepo: UserRepo) {}
 
   async execute(id: string, data: UserUpdateDTO) {
-    const isUsernameExists = Boolean(
-      await this.userRepo.getById(id)
-    );
-
-    if (!isUsernameExists) throw new UserNotFoundError(id)
-
-    const result = await this.userRepo.getById(id)
-    const user = result.data
-
-    user.update(data)
-    return this.userRepo.update(id, user)
+    return this.userRepo.update(id, data)
   }
 }
