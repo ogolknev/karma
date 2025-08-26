8d80c033b9490199d2e68e11d0ac77bd4d5177b1
ogolknev <ogolknevnv@ya.ru>
2025-08-26 15:28:11 +0300

    refactor(core, user): unify findByWorkspace and findBySearch into find

--- src/core/modules/user/user.repo.ts
+++ src/core/modules/user/user.repo.ts
@@ -8,11 +8,7 @@ export interface UserRepo {
   getById(id: string): Promise<RepoResult<User | null>>;
   getByUsername(username: string): Promise<RepoResult<User | null>>;
 
-  findByWorkspace(
-    workspaceId: string,
-    options: FindOptions
-  ): Promise<FindResult<User>>;
-  findBySearch(query: string, options: FindOptions): Promise<FindResult<User>>;
+  find(query?: string, options?: FindOptions): Promise<FindResult<User>>;
 
   update(id: string, data: UserUpdateDTO): Promise<RepoResult<User | null>>;
 
