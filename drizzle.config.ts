38eca97e55648f2c9b4e713cf10ab21e636770b7
Ogolknev Nikita <ogolknevnv@ya.ru>
2025-09-21 10:00:06 +0300

    chore: update dependencies, ts and drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/infrastructure/db/drizzle/schema.ts",
+  schema: "./src/infrastructure/db/drizzle/pg/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
     url: config.env.DATABASE_URL,
