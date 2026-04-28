2add91b0080cc194d7f4bd028bf0b62e54de036e
ogolknev <ogolknevnv@ya.ru>
2026-04-28 16:24:55 +0300

    chore(config): add email verification env config

--- src/shared/config/env.config.ts
+++ src/shared/config/env.config.ts
@@ -1,19 +1,31 @@
 import { config } from "dotenv";
 import { EnvError } from "../errors";
 
-config()
+config();
 
-function loadEnv(key: string): string {
-  const value = process.env[key];
+type LoadEnvOptions = {
+    required?: boolean;
+};
+function loadEnv(key: string, options?: { required: true }): string;
+function loadEnv(key: string, options?: { required: false }): string | undefined;
+function loadEnv(
+    key: string,
+    { required = true }: LoadEnvOptions = { required: true },
+): string | undefined {
+    const value = process.env[key];
 
-  if (!value) {
-    throw new EnvError(key);
-  }
+    if (!value && required) {
+        throw new EnvError(key);
+    }
 
-  return value;
+    return value;
 }
 
 export const env = {
-  DATABASE_URL: loadEnv("DATABASE_URL"),
-  TEST_DATABASE_URL: loadEnv("TEST_DATABASE_URL"),
+    DATABASE_URL: loadEnv("DATABASE_URL"),
+    TEST_DATABASE_URL: loadEnv("TEST_DATABASE_URL"),
+    EMAIL_FROM: loadEnv("EMAIL_FROM"),
+    EMAIL_VERIFICATION_CODE_EXPIRATION_TIME: parseInt(
+        loadEnv("EMAIL_VERIFICATION_CODE_EXPIRATION_TIME"),
+    ),
 };
